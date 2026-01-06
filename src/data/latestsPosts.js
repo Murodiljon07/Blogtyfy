e95016e634605de3b032d7cf5a8e3d22d6ef8bc2
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-01-07 01:40:57 +0500

    al is done

--- src/data/latestsPosts.js
+++ src/data/latestsPosts.js
@@ -1,7 +1,6 @@
 const latestsPosts = [
   {
     id: 1,
-    Image: "/images/latest-post-1.png",
     date: "Nov 20, 2025",
     title: "The Future of Web Development",
     description: `Exploring the latest trends and technologies
@@ -9,7 +8,6 @@ shaping the future of web development in 2024.`,
   },
   {
     id: 2,
-    Image: "/images/latest-post-1.png",
     date: "Nov 18, 2025",
     title: "Mastering Productivity",
     description: `Proven strategies and tools to boost your
@@ -17,7 +15,6 @@ productivity and achieve your goals faster.`,
   },
   {
     id: 3,
-    Image: "/images/latest-post-1.png",
     date: "Nov 15, 2025",
     title: "Design Principles That Matter",
     description: `Essential design principles every creator should
