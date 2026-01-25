21d5a414aaad574329bc5e1c4a747529bae6d8e5
Murodiljon07 <murodiljonabdumutalovtg1@gmail.com>
2026-01-25 11:31:57 +0500

    add posts page

--- src/Components/IndexCard.jsx
+++ src/Components/IndexCard.jsx
@@ -1,17 +1,53 @@
 import React from "react";
-
-let cardInfoType = [];
+import { GrDocumentText } from "react-icons/gr";
+import { PiChartLineUpLight } from "react-icons/pi";
+import { FiUsers } from "react-icons/fi";
 
 function IndexCard({ totalPosts, category }) {
-  return (
-    <div>
-      <div>
-        <img src="" alt="" />
-        <span>+8%</span>
+  let cardInfoType = [
+    {
+      icon: <GrDocumentText />,
+      degree: "12%",
+      title: "Total Posts",
+      index: totalPosts,
+    },
+    {
+      icon: <GrDocumentText />,
+      degree: "12%",
+      title: "Total Posts",
+      index: totalPosts,
+    },
+    {
+      icon: <PiChartLineUpLight />,
+      degree: "12%",
+      title: "Total Posts",
+      index: totalPosts,
+    },
+    {
+      icon: <FiUsers />,
+      degree: "12%",
+      title: "Total Posts",
+      index: totalPosts,
+    },
+  ];
+
+  return cardInfoType.map((item) => {
+    let { icon, degree, title, index } = item;
+    return (
+      <div className="border border-gray-300 rounded-[12px] w-[302px] h-[170px] p-[25px]">
+        <div className="flex w-full justify-between items-center">
+          <div className="h-[48px] w-[48px] text-[20px] bg-[#4346EF1A] flex items-center justify-center rounded-[12px]">
+            {icon}
+          </div>
+          <span className="text-[12px] font-medium text-green-600">
+            {degree}
+          </span>
+        </div>
+        <h4>{index}</h4>
+        <p>{title}</p>
       </div>
-      <h4>1.2K</h4>
-    </div>
-  );
+    );
+  });
 }
 
 export default IndexCard;
